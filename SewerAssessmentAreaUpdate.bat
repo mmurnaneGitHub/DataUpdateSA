eaf96f22543a6eec5047900a6b099f3c0c08d937
Mike Murnane <mmurnane@cityoftacoma.org>
2019-10-08 16:24:13 -0700

    Upload file

---
+++ SewerAssessmentAreaUpdate.bat
@@ -0,0 +1,7 @@
+::Update Sewer Assessment Areas
+
+Echo Updating Sewer Assessment Areas ...
+ XCOPY \\fs005\eng_comm\PUBLIC\JShell\GIS\SewerAssessmentAreas\SewerAssessmentAreas.gdb \\fs005\ArcGISStore\data\IT_GIS\SewerAssessmentAreas.gdb /E /I /H /R /Y
+
+::For manual testing -
+pause
